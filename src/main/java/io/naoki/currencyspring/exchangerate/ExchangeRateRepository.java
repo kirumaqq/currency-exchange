cb398d782cca143c9af5c4075904825099851693
Umid <umid1504@gmail.com>
2024-01-19 19:36:19 +0500

    feat: make ExchangeRateRepository with ExchangeRateRowMapper covered with tests

---
+++ src/main/java/io/naoki/currencyspring/exchangerate/ExchangeRateRepository.java
@@ -0,0 +1,62 @@
+package io.naoki.currencyspring.exchangerate;
+
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+public interface ExchangeRateRepository extends Repository<ExchangeRate, Integer> {
+
+    @Query(value = """
+            SELECT er.id as id, er.rate as rate,
+                   er.id as id, er.rate as rate,
+                   base.id as base_id, base.code as base_code,
+                   base.full_name as base_name, base.sign as base_sign,
+                   target.id as target_id, target.code as target_code,
+                   target.full_name as target_name, target.sign as target_sign
+            FROM exchange_rates er
+            JOIN public.currencies base on base.id = er.base_currency_id
+            JOIN public.currencies target on target.id = er.target_currency_id
+            """, rowMapperClass = ExchangeRateRowMapper.class)
+    List<ExchangeRate> findAll();
+
+    @Query(value = """
+            SELECT er.id as id, er.rate as rate,
+                   er.id as id, er.rate as rate,
+                   base.id as base_id, base.code as base_code,
+                   base.full_name as base_name, base.sign as base_sign,
+                   target.id as target_id, target.code as target_code,
+                   target.full_name as target_name, target.sign as target_sign
+            FROM exchange_rates er
+            JOIN public.currencies base on base.id = er.base_currency_id
+            JOIN public.currencies target on target.id = er.target_currency_id
+            WHERE
+                base.code = :baseCode AND
+                target.code = :targetCode
+            """, rowMapperClass = ExchangeRateRowMapper.class)
+    Optional<ExchangeRate> findByPairCodes(String baseCode, String targetCode);
+
+    @Query("""
+            INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+            VALUES (:fromId, :toId, :rate)
+            RETURNING id
+            """)
+    Integer save(Integer fromId, Integer toId, BigDecimal rate);
+
+    @Query(value = """
+            UPDATE exchange_rates er
+            SET rate = :rate
+            FROM currencies base, currencies target
+            WHERE
+                base.code = :baseCode AND target.code = :targetCode AND
+                base.id = base_currency_id AND target.id = target_currency_id
+            RETURNING er.id as id, er.rate as rate,
+                base.id as base_id, base.code as base_code,
+                base.full_name as base_name, base.sign as base_sign,
+                target.id as target_id, target.code as target_code,
+                target.full_name as target_name, target.sign as target_sign
+            """, rowMapperClass = ExchangeRateRowMapper.class)
+    Optional<ExchangeRate> updateByPairCodes(String baseCode, String targetCode, BigDecimal rate);
+}
---
+++ src/test/java/io/naoki/currencyspring/exchangerate/ExchangeRateRepositoryIT.java
@@ -0,0 +1,102 @@
+package io.naoki.currencyspring.exchangerate;
+
+import io.naoki.currencyspring.TestCurrencySpringApplication;
+import io.naoki.currencyspring.currency.Currency;
+import io.naoki.currencyspring.currency.CurrencyRepository;
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@Transactional
+@SpringBootTest(classes = TestCurrencySpringApplication.class)
+class ExchangeRateRepositoryIT {
+
+    private static final Offset<BigDecimal> delta = within(BigDecimal.valueOf(0.000000001));
+
+    @Autowired
+    ExchangeRateRepository exchangeRateRepository;
+
+    @Autowired
+    CurrencyRepository currencyRepository;
+
+    @Test
+    void findAll_DoesNotThrow_ListSizeIsNotZero() {
+        saveExchangeRate();
+
+        List<ExchangeRate> exchangeRateList = exchangeRateRepository.findAll();
+
+        assertThat(exchangeRateList).hasSizeGreaterThan(0);
+    }
+
+    @Test
+    void findByPairCodes_ValidPair_ReturnsValidExchangeRate() {
+        ExchangeRate origin = saveExchangeRate();
+
+        ExchangeRate result = exchangeRateRepository.findByPairCodes(
+                origin.baseCurrency().code(), origin.targetCurrency().code()).orElseThrow();
+
+        assertThat(result).isEqualTo(origin);
+    }
+
+    @Test
+    void findByPairCodes_InvalidPair_ReturnsEmptyOptional() {
+
+        Optional<ExchangeRate> exchangeRateOpt = exchangeRateRepository.findByPairCodes("AAA", "BBB");
+
+        assertThat(exchangeRateOpt).isEmpty();
+    }
+
+    @Test
+    void save_returnsNotNullId() {
+        Currency savedFrom = saveCurrency("AAA");
+        Currency savedTo = saveCurrency("BBB");
+
+        Integer id = assertDoesNotThrow(() ->
+                exchangeRateRepository.save(savedFrom.id(), savedTo.id(), BigDecimal.ONE));
+
+        assertThat(id).isNotNull();
+    }
+
+    @Test
+    void updateByPairCodes_ValidPair_ReturnsUpdatedExchangeRate() {
+        ExchangeRate origin = saveExchangeRate();
+
+        ExchangeRate updated = exchangeRateRepository
+                .updateByPairCodes(origin.baseCurrency().code(), origin.targetCurrency().code(), BigDecimal.TEN)
+                .orElseThrow();
+
+        ExchangeRate result = exchangeRateRepository.findByPairCodes(origin.baseCurrency().code(),
+                origin.targetCurrency().code()).orElseThrow();
+        assertThat(updated).isEqualTo(result);
+        assertThat(updated.rate()).isCloseTo(BigDecimal.TEN, delta);
+    }
+
+    @Test
+    void updateByPairCodes_InvalidPair_returnsEmptyOptional() {
+        Optional<ExchangeRate> exchangeRateOpt = exchangeRateRepository.updateByPairCodes("AAA", "BBB", BigDecimal.ONE);
+
+        assertThat(exchangeRateOpt).isEmpty();
+    }
+
+    Currency saveCurrency(String currencyCode) {
+        return currencyRepository.save(new Currency(null, currencyCode, "name", "s"));
+    }
+
+    ExchangeRate saveExchangeRate() {
+        Currency base = saveCurrency("AAA");
+        Currency target = saveCurrency("BBB");
+        int id = exchangeRateRepository.save(base.id(), target.id(), BigDecimal.ONE);
+        return new ExchangeRate(id, base, target, BigDecimal.ONE.setScale(6, RoundingMode.CEILING));
+    }
+}
