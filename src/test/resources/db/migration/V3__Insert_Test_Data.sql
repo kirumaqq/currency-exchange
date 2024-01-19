d10e70f3185e60324fe3ceb57c9ab1c77cfc4cac
Umid <umid1504@gmail.com>
2024-01-19 11:44:12 +0500

    test: add insert scripts

---
+++ src/test/resources/db/migration/V3__Insert_Test_Data.sql
@@ -0,0 +1,283 @@
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Leke', 'ALL', 'Lek');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Dollars', 'USD', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Afghanis', 'AFN', '؋');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Pesos', 'ARS', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Guilders', 'AWG', 'ƒ');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Dollars', 'AUD', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('New Manats', 'AZN', 'ман');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Dollars', 'BSD', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Dollars', 'BBD', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Rubles', 'BYR', 'p.');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Euro', 'EUR', '€');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Dollars', 'BZD', 'BZ$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Dollars', 'BMD', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Bolivianos', 'BOB', '$b');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Convertible Marka', 'BAM', 'KM');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Pula', 'BWP', 'P');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Leva', 'BGN', 'лв');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Reais', 'BRL', 'R$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Pounds', 'GBP', '£');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Dollars', 'BND', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Riels', 'KHR', '៛');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Dollars', 'CAD', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Dollars', 'KYD', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Pesos', 'CLP', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Yuan Renminbi', 'CNY', '¥');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Pesos', 'COP', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Colón', 'CRC', '₡');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Kuna', 'HRK', 'kn');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Pesos', 'CUP', '₱');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Koruny', 'CZK', 'Kč');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Kroner', 'DKK', 'kr');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Pesos', 'DOP', 'RD$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Dollars', 'XCD', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Pounds', 'EGP', '£');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Colones', 'SVC', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Pounds', 'FKP', '£');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Dollars', 'FJD', '$');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Cedis', 'GHC', '¢');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Pounds', 'GIP', '£');
+INSERT INTO currencies (full_name, code, sign)
+VALUES ('Quetzales', 'GTQ', 'Q');
+
+
+
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (4, 32, 8641.803);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (18, 6, 5993.705);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (21, 26, 2243.116);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (5, 29, 905.024);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (24, 6, 9149.52);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (29, 13, 7775.998);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (24, 14, 5604.84);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (33, 36, 3377.098);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (35, 11, 1746.321);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (28, 9, 5450.305);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (23, 14, 511.097);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (38, 29, 5291.273);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (20, 30, 4505.153);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (6, 34, 2851.898);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (36, 4, 794.334);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (24, 30, 9370.024);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (7, 21, 1736.07);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (31, 29, 7236.658);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (13, 7, 457.534);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (27, 22, 8331.896);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (12, 15, 7232.236);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (16, 28, 3673.599);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (36, 40, 4742.238);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (18, 10, 8908.146);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (19, 11, 1478.201);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (30, 7, 4922.635);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (16, 37, 53.169);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (32, 23, 7188.585);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (35, 31, 1043.395);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (11, 31, 9336.753);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (32, 4, 5376.284);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (11, 25, 4017.894);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (26, 14, 4967.748);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (14, 28, 9320.256);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (39, 18, 801.194);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (39, 40, 3230.393);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (2, 17, 507.646);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (5, 7, 3641.419);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (26, 39, 2661.07);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (8, 13, 5219.781);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (35, 5, 3174.112);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (12, 13, 1146.604);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (31, 28, 8166.445);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (30, 9, 4908.717);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (21, 15, 2308.432);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (31, 9, 3808.717);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (12, 20, 5587.616);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (29, 1, 212.861);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (22, 8, 1115.57);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (39, 15, 3346.744);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (18, 38, 3991.521);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (30, 24, 9672.803);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (6, 9, 8985.795);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (20, 33, 1171.737);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (21, 35, 9762.09);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (40, 34, 23.935);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (3, 14, 2567.882);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (10, 39, 5130.819);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (21, 21, 5204.26);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (35, 3, 7957.808);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (22, 22, 1153.113);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (17, 37, 4975.097);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (8, 4, 9060.357);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (4, 33, 6330.374);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (2, 19, 7724.903);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (37, 33, 3368.408);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (12, 23, 4011.621);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (17, 14, 1460.285);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (14, 10, 1671.347);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (5, 10, 9108.048);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (3, 28, 6260.635);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (4, 30, 8267.844);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (37, 29, 9152.163);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (23, 7, 6632.094);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (15, 32, 3729.789);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (37, 32, 5628.731);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (31, 27, 8462.645);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (36, 33, 6562.3);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (25, 35, 9312.667);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (6, 23, 2619.403);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (36, 24, 5450.356);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (17, 13, 3775.61);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (1, 4, 7034.041);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (26, 16, 4247.766);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (12, 25, 5366.171);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (25, 19, 4904.257);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (36, 5, 1071.832);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (39, 6, 5444.58);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (3, 15, 1032.043);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (7, 18, 9000.52);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (1, 16, 7734.986);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (12, 16, 5014.975);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (26, 30, 229.97);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (2, 8, 556.115);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (14, 32, 529.163);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (16, 30, 3654.173);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (5, 15, 3436.545);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (1, 39, 6888.802);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (23, 22, 3886.872);
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (23, 21, 3886.872);
