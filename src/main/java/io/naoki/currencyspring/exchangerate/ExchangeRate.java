bec8520d8ad6536b19adf16b93131f94d045a84b
Umid <umid1504@gmail.com>
2024-01-19 14:31:48 +0500

    chore: remove redundant annotations on ExchangeRate

--- src/main/java/io/naoki/currencyspring/exchangerate/ExchangeRate.java
+++ src/main/java/io/naoki/currencyspring/exchangerate/ExchangeRate.java
@@ -1,7 +1,6 @@
 package io.naoki.currencyspring.exchangerate;
 
 import io.naoki.currencyspring.currency.Currency;
-import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
@@ -9,9 +8,7 @@ import java.math.BigDecimal;
 @Table("exchange_rates")
 public record ExchangeRate(
         Integer id,
-        @Column("base_currency_id")
         Currency baseCurrency,
-        @Column("target_currency_id")
         Currency targetCurrency,
         BigDecimal rate
 
