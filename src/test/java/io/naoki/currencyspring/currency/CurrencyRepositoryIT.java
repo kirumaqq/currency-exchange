191ba3c2af4286d2f17eb5eb3f8d743e90a18452
Umid <umid1504@gmail.com>
2024-01-19 11:51:49 +0500

    test: cover CurrencyRepository with test

---
+++ src/test/java/io/naoki/currencyspring/currency/CurrencyRepositoryIT.java
@@ -0,0 +1,50 @@
+package io.naoki.currencyspring.currency;
+
+import io.naoki.currencyspring.DatabaseCleanupExecutionListener;
+import io.naoki.currencyspring.TestCurrencySpringApplication;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@TestExecutionListeners(listeners = {DatabaseCleanupExecutionListener.class},
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
+@SpringBootTest(classes = TestCurrencySpringApplication.class)
+class CurrencyRepositoryIT {
+
+    @Autowired
+    Flyway flyway;
+
+    @Autowired
+    CurrencyRepository currencyRepository;
+
+    @Test
+    void findAll_DoesNotThrow() {
+        Currency currency = new Currency(null, "TST", "Test", "T");
+        currencyRepository.save(currency);
+        List<Currency> currencyList = assertDoesNotThrow(currencyRepository::findAll);
+        assertThat(currencyList).hasSizeGreaterThan(0);
+    }
+
+    @Test
+    void findByCode_ValidCode_DoesNotThrow() {
+        Currency currency = new Currency(null, "TST", "Test", "T");
+        currencyRepository.save(currency);
+        Currency savedCurrency = currencyRepository.findByCode(currency.code()).orElseThrow();
+        assertThat(savedCurrency.fullName()).isEqualTo(currency.fullName());
+    }
+
+
+    @Test
+    void save_ReturnsEntityWithId() {
+        Currency currency = new Currency(null, "TST", "Test", "T");
+        Currency savedCurrency = currencyRepository.save(currency);
+        assertThat(savedCurrency.id()).isNotNull();
+    }
+}
