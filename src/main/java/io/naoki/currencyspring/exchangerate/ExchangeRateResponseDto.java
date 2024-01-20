0e1ff568bacf0607cb7b38a3700889ad1df7555b
Umid <umid1504@gmail.com>
2024-01-20 16:48:08 +0500

    fix: rate rounding for ExchangeRateResponseDto

--- src/main/java/io/naoki/currencyspring/exchangerate/ExchangeRateResponseDto.java
+++ src/main/java/io/naoki/currencyspring/exchangerate/ExchangeRateResponseDto.java
@@ -3,6 +3,7 @@ package io.naoki.currencyspring.exchangerate;
 import io.naoki.currencyspring.currency.CurrencyResponseDto;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public record ExchangeRateResponseDto(
         Integer id,
@@ -10,4 +11,11 @@ public record ExchangeRateResponseDto(
         CurrencyResponseDto targetCurrency,
         BigDecimal rate
 ) {
+
+    public ExchangeRateResponseDto(Integer id, CurrencyResponseDto baseCurrency, CurrencyResponseDto targetCurrency, BigDecimal rate) {
+        this.id = id;
+        this.baseCurrency = baseCurrency;
+        this.targetCurrency = targetCurrency;
+        this.rate = rate.setScale(2, RoundingMode.HALF_EVEN);
+    }
 }
