88fb8f5df8b0388413d8dbf60ee6c1d2336cd440
Umid <umid1504@gmail.com>
2024-01-21 10:45:35 +0500

    feat: add field constraints on DTOs

--- src/main/java/io/naoki/currencyspring/dto/currency/CreateCurrencyDto.java
+++ src/main/java/io/naoki/currencyspring/dto/currency/CreateCurrencyDto.java
@@ -1,8 +1,14 @@
 package io.naoki.currencyspring.dto.currency;
 
+import io.naoki.currencyspring.infrastructure.ISO4217;
+import jakarta.validation.constraints.Size;
+
 public record CreateCurrencyDto(
+        @Size(min = 3, max = 40, message = "Currency name length must not be less than 3 and greater than 40")
         String name,
+        @ISO4217
         String code,
+        @Size(min = 1, max = 10, message = "Currency name length must not be less than 1 and greater than 10")
         String sign
 ) {
 }
--- src/main/java/io/naoki/currencyspring/dto/exchangerate/CreateExchangeRateDto.java
+++ src/main/java/io/naoki/currencyspring/dto/exchangerate/CreateExchangeRateDto.java
@@ -1,10 +1,16 @@
 package io.naoki.currencyspring.dto.exchangerate;
 
+import io.naoki.currencyspring.infrastructure.ISO4217;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 
 public record CreateExchangeRateDto(
+        @ISO4217
         String baseCurrencyCode,
+        @ISO4217
         String targetCurrencyCode,
+        @Positive
         BigDecimal rate
 ) {
 }
