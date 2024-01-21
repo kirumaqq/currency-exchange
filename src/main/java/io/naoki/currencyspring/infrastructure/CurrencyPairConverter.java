014dbcb91a392d42cedd0e1ce477df01faebc760
Umid <umid1504@gmail.com>
2024-01-21 10:42:09 +0500

    feat: apply currency pair validation on source string in CurrencyPairConverter

--- src/main/java/io/naoki/currencyspring/infrastructure/CurrencyPairConverter.java
+++ src/main/java/io/naoki/currencyspring/infrastructure/CurrencyPairConverter.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 public class CurrencyPairConverter implements Converter<String, CurrencyPair> {
     @Override
     public CurrencyPair convert(String source) {
+        if (source.length() != 6) throw new IllegalArgumentException("Both currencies codes must be equal to 3");
         String base = source.substring(0, 3);
         String target = source.substring(3);
         return new CurrencyPair(base.toUpperCase(), target.toUpperCase());
