cf5278d8ff4b268a26d13cbd095bc9fe6493e1a8
Umid <umid1504@gmail.com>
2024-02-06 11:29:42 +0500

    feat: Dividing context as constant for implementations of ConversionStrategy

--- src/main/java/io/naoki/currencyspring/service/ConversionStrategy.java
+++ src/main/java/io/naoki/currencyspring/service/ConversionStrategy.java
@@ -2,9 +2,14 @@ package io.naoki.currencyspring.service;
 
 import io.naoki.currencyspring.entity.ExchangeRate;
 
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.Optional;
 
 public interface ConversionStrategy {
+
+    MathContext DIVIDING_CONTEXT = new MathContext(6, RoundingMode.HALF_EVEN);
+
 
     Optional<ExchangeRate> convert(String from, String to);
 }
