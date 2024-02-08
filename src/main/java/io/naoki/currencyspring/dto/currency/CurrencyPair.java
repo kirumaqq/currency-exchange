589dcd22bfde748a3e32d14472c6473d6e511d51
Umid <umid1504@gmail.com>
2024-02-08 12:03:45 +0500

    refactor: validate with Validator object instead of doing it manually

--- src/main/java/io/naoki/currencyspring/dto/currency/CurrencyPair.java
+++ src/main/java/io/naoki/currencyspring/dto/currency/CurrencyPair.java
@@ -1,7 +1,11 @@
 package io.naoki.currencyspring.dto.currency;
 
+import io.naoki.currencyspring.infrastructure.ISO4217;
+
 public record CurrencyPair(
+        @ISO4217
         String baseCurrencyCode,
+        @ISO4217
         String targetCurrencyCode
 ) {
 }
